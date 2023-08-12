b6ce7a158e685691bdfa3b827e1e99e54141e501
coldrain-f <intmain__@naver.com>
2023-08-12 21:52:38 +0900

    Chore: 미사용 Import 제거

--- awesomeNativeBase/App.tsx
+++ awesomeNativeBase/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from './src/pages/navigation';
-import {NativeBaseProvider, StatusBar} from 'native-base';
+import {NativeBaseProvider} from 'native-base';
 
 export default function App(): React.JSX.Element {
   return (
