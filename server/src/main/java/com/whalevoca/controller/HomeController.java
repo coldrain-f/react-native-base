610b9dd3d9928ad8d34be11e2793d42997021186
coldrain-f <intmain__@naver.com>
2023-09-30 21:47:50 +0900

    Add: 스프링 부트 서버 기본 설정 추가

--- server/src/main/java/com/whalevoca/controller/HomeController.java
+++ server/src/main/java/com/whalevoca/controller/HomeController.java
@@ -1,2 +1,13 @@
-package com.whalevoca.controller;public class HomeController {
+package com.whalevoca.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 }
