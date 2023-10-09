775103579fb6254eccf538ac0ce1a49b5c339e5e
coldrain-f <intmain__@naver.com>
2023-10-09 22:17:29 +0900

    Feat: 단어장 관리 화면 기본 틀 개발 [#18]

---
+++ dashboard/src/main/java/whale/dashboard/controller/DashboardController.java
@@ -0,0 +1,15 @@
+package whale.dashboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dashboard")
+public class DashboardController {
+
+    @GetMapping("/vocabulary")
+    public String main() {
+        return "dashboard/vocabulary";
+    }
+}
