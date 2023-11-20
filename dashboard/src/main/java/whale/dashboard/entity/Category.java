b06e908065fe7b85cd89f209b2b56cb69cbf5fd0
coldrain-f <intmain__@naver.com>
2023-11-20 19:56:48 +0900

    Feat: Category 엔티티 작성 [#22]

---
+++ dashboard/src/main/java/whale/dashboard/entity/Category.java
@@ -0,0 +1,22 @@
+package whale.dashboard.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CATEGORY_ID")
+    private Long id;
+
+    private String subject;
+
+    private String description;
+}
