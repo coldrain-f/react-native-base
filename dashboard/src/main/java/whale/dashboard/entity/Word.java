2b610807ac1fa06ce819741659bf0a003e09807c
coldrain-f <intmain__@naver.com>
2023-11-20 20:03:37 +0900

    Feat: Word 엔티티 작성 [#22]

---
+++ dashboard/src/main/java/whale/dashboard/entity/Word.java
@@ -0,0 +1,22 @@
+package whale.dashboard.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Word {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "WORD_ID")
+    private Long id;
+
+    private String name;
+
+    private String meaning;
+}
