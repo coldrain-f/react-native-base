7885286e06373edf775ee27ce175cb0a30799594
coldrain-f <intmain__@naver.com>
2023-12-04 20:26:05 +0900

    Feat: Yomi 엔티티 추가 [#22]

---
+++ dashboard/src/main/java/whale/dashboard/entity/Yomi.java
@@ -0,0 +1,41 @@
+package whale.dashboard.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Yomi {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "YOMI_ID")
+    private Long id;
+
+    @Column
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "KANJI_ID")
+    private Kanji kanji;
+
+    @ManyToOne
+    @JoinColumn(name = "YOMI_TYPE_ID")
+    private YomiType yomiType;
+
+    @Builder
+    public Yomi(final String name, final Kanji kanji, final YomiType yomiType) {
+        this.name = name;
+        this.kanji = kanji;
+        this.yomiType = yomiType;
+    }
+
+    public void change(final String name) {
+        this.name = name;
+    }
+}
