d0c6f92bc72b6c55e81e2bd24ef087b8baa054ab
Ksinny <dosly2@naver.com>
2023-12-19 22:11:26 +0900

    Feat: 단어장 DTO 생성 [#39]

---
+++ dashboard/src/main/java/whale/dashboard/dto/VocabularyDto.java
@@ -0,0 +1,79 @@
+package whale.dashboard.dto;
+
+import lombok.*;
+import whale.dashboard.entity.Vocabulary;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class VocabularyDto {
+
+    @Data
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class RegistrationRequest {
+
+        @NotBlank(message = "Title을 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "Description을 입력해주세요.")
+        private String description;
+
+        public RegistrationRequest(String title, String description) {
+            this.title = title;
+            this.description = description;
+        }
+    }
+
+
+    @Data
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ModifyRequest {
+
+        @NotNull(message = "수정할 ID를 입력해주세요.")
+        private Long id;
+
+        @NotBlank(message = "Title을 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "Description을 입력해주세요.")
+        private String description;
+
+        public ModifyRequest(Long id, String title, String description) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Response {
+        private Long id;
+        private String title;
+        private String description;
+
+
+        @Builder
+        public Response(Long id, String title, String description) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+        }
+
+
+        public static List<Response> toList(List<Vocabulary> vocabularies) {
+            return vocabularies.stream()
+                    .map(vocabulary -> Response.builder()
+                            .id(vocabulary.getId())
+                            .title(vocabulary.getTitle())
+                            .description(vocabulary.getDescription())
+                            .build())
+                    .collect(Collectors.toList());
+        }
+
+    }
+
+}
