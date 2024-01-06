2d594175677f4acb07b7cfb1792e9f8244f8799a
Ksinny <dosly2@naver.com>
2024-01-06 23:34:45 +0900

    Feat: Furigana API에 유효성 검사 애노테이션 적용 [#39]

--- dashboard/src/main/java/whale/dashboard/controller/FuriganaApiController.java
+++ dashboard/src/main/java/whale/dashboard/controller/FuriganaApiController.java
@@ -5,16 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import whale.dashboard.dto.DeleteIdListRequest;
 import whale.dashboard.dto.FuriganaDto;
 import whale.dashboard.service.FuriganaService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Validated
 public class FuriganaApiController {
 
     private final FuriganaService furiganaService;
@@ -22,20 +25,21 @@ public class FuriganaApiController {
     @PostMapping("/words/{id}/furigana")
     public ResponseEntity<Void> register(
             @PathVariable Long id,
-            @RequestBody List<FuriganaDto.RegistrationRequest> requests) {
+            @RequestBody @Valid List<FuriganaDto.RegistrationRequest> requests) {
         furiganaService.registerFurigana(id, requests);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/furigana")
-    public ResponseEntity<Void> modify(@RequestBody List<FuriganaDto.ModifyRequest> requests) {
+    public ResponseEntity<Void> modify(
+            @RequestBody @Valid List<FuriganaDto.ModifyRequest> requests) {
         furiganaService.modifyFurigana(requests);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/furigana")
     public ResponseEntity<Void> remove(
-            @RequestBody DeleteIdListRequest request) {
+            @RequestBody @Valid DeleteIdListRequest request) {
         furiganaService.removeFurigana(request.getIdList());
         return ResponseEntity.ok().build();
     }
