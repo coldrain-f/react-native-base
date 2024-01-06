01934b30cf5ac09818c55d8069a4844e7676be13
Ksinny <dosly2@naver.com>
2024-01-06 23:09:55 +0900

    Feat: Vocabulary API에 유효성 검사 애노테이션 적용 [#39]

--- dashboard/src/main/java/whale/dashboard/controller/VocabularyApiController.java
+++ dashboard/src/main/java/whale/dashboard/controller/VocabularyApiController.java
@@ -5,23 +5,26 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import whale.dashboard.dto.DeleteIdListRequest;
 import whale.dashboard.dto.VocabularyDto;
 import whale.dashboard.service.VocabularyService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/vocabulary")
+@Validated
 public class VocabularyApiController {
 
     private final VocabularyService vocabularyService;
 
     @PostMapping
     public ResponseEntity<Void> register(
-            @RequestBody List<VocabularyDto.RegistrationRequest> requests) {
+             @RequestBody @Valid List<VocabularyDto.RegistrationRequest> requests) {
         vocabularyService.registerVocabulary(requests);
         return ResponseEntity.ok().build();
     }
@@ -29,7 +32,7 @@ public class VocabularyApiController {
 
     @PatchMapping
     public ResponseEntity<Void> modify(
-            @RequestBody List<VocabularyDto.ModifyRequest> requests) {
+            @RequestBody @Valid List<VocabularyDto.ModifyRequest> requests) {
         vocabularyService.modifyVocabulary(requests);
         return ResponseEntity.ok().build();
     }
