2166d284feb8a9fabbf7ac874cd6e4aeda930eb7
Ksinny <dosly2@naver.com>
2024-01-06 23:34:02 +0900

    Feat: Word API에 유효성 검사 애노테이션 적용 [#39]

--- dashboard/src/main/java/whale/dashboard/controller/WordApiController.java
+++ dashboard/src/main/java/whale/dashboard/controller/WordApiController.java
@@ -5,16 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import whale.dashboard.dto.DeleteIdListRequest;
 import whale.dashboard.dto.WordDto;
 import whale.dashboard.service.WordService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Validated
 public class WordApiController {
 
     private final WordService wordService;
@@ -22,20 +25,21 @@ public class WordApiController {
     @PostMapping("/yomi/{id}/words")
     public ResponseEntity<Void> register(
             @PathVariable Long id,
-            @RequestBody List<WordDto.RegistrationRequest> requests) {
+            @RequestBody @Valid List<WordDto.RegistrationRequest> requests) {
         wordService.registerWords(id, requests);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/words")
-    public ResponseEntity<Void> modify(@RequestBody List<WordDto.ModifyRequest> requests) {
+    public ResponseEntity<Void> modify(
+            @RequestBody @Valid List<WordDto.ModifyRequest> requests) {
         wordService.modifyWords(requests);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/words")
     public ResponseEntity<Void> remove(
-            @RequestBody DeleteIdListRequest request) {
+            @RequestBody @Valid DeleteIdListRequest request) {
         wordService.removeWords(request.getIdList());
         return ResponseEntity.ok().build();
     }
