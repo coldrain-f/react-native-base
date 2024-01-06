880871e22f914f984b91fe929f62ccffe2bd3789
Ksinny <dosly2@naver.com>
2024-01-06 23:41:13 +0900

    Feat: DeletedIdListRequest에 유효성 검사 애노테이션 추가 [#39]

--- dashboard/src/main/java/whale/dashboard/dto/DeleteIdListRequest.java
+++ dashboard/src/main/java/whale/dashboard/dto/DeleteIdListRequest.java
@@ -1,12 +1,16 @@
 package whale.dashboard.dto;
 
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @NoArgsConstructor
 public class DeleteIdListRequest {
+
+    @NotNull(message = "삭제할 ID를 입력해주세요.")
+    @Size(min = 1, message = "하나 이상의 ID를 입력해주세요.")
     private List<Long> idList;
 
     public DeleteIdListRequest(List<Long> idList) {
