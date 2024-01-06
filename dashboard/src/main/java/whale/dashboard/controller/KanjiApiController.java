e8565e492a96c61dc14a4ad5e605f0744989fc19
Ksinny <dosly2@naver.com>
2024-01-06 23:32:00 +0900

    Feat: Kanji API에 유효성 검사 애노테이션 적용 [#39]

--- dashboard/src/main/java/whale/dashboard/controller/KanjiApiController.java
+++ dashboard/src/main/java/whale/dashboard/controller/KanjiApiController.java
@@ -5,16 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import whale.dashboard.dto.DeleteIdListRequest;
 import whale.dashboard.dto.KanjiDto;
 import whale.dashboard.service.KanjiService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Validated
 public class KanjiApiController {
 
     private final KanjiService kanjiService;
@@ -22,20 +25,21 @@ public class KanjiApiController {
     @PostMapping("/categories/{id}/kanjis")
     public ResponseEntity<Void> register(
             @PathVariable Long id,
-            @RequestBody List<KanjiDto.RegistrationRequest> requests) {
+            @RequestBody @Valid List<KanjiDto.RegistrationRequest> requests) {
         kanjiService.registerKanji(id, requests);
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/kanjis")
-    public ResponseEntity<Void> modify(@RequestBody List<KanjiDto.ModifyRequest> requests) {
+    public ResponseEntity<Void> modify(
+            @RequestBody @Valid List<KanjiDto.ModifyRequest> requests) {
         kanjiService.modifyKanjis(requests);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/kanjis")
     public ResponseEntity<Void> remove(
-            @RequestBody DeleteIdListRequest request) {
+            @RequestBody @Valid DeleteIdListRequest request) {
         kanjiService.removeKanjis(request.getIdList());
         return ResponseEntity.ok().build();
     }
