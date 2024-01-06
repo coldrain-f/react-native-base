853aee40d5b5ca778b3d62779423882ced6e91a3
Ksinny <dosly2@naver.com>
2024-01-06 23:14:14 +0900

    Feat: Category API에 유효성 검사 애노테이션 적용 [#39]

--- dashboard/src/main/java/whale/dashboard/controller/CategoryApiController.java
+++ dashboard/src/main/java/whale/dashboard/controller/CategoryApiController.java
@@ -5,16 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import whale.dashboard.dto.CategoryDto;
 import whale.dashboard.dto.DeleteIdListRequest;
 import whale.dashboard.service.CategoryService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Validated
 public class CategoryApiController {
 
     private final CategoryService categoryService;
@@ -22,14 +25,15 @@ public class CategoryApiController {
     @PostMapping("/vocabulary/{id}/categories")
     public ResponseEntity<Void> register(
             @PathVariable Long id,
-            @RequestBody List<CategoryDto.RegistrationRequest> requests) {
+            @RequestBody @Valid List<CategoryDto.RegistrationRequest> requests) {
         categoryService.registerCategory(id, requests);
         return ResponseEntity.ok().build();
     }
 
 
     @PatchMapping("/categories")
-    public ResponseEntity<Void> modify(@RequestBody List<CategoryDto.ModifyRequest> requests) {
+    public ResponseEntity<Void> modify(
+            @RequestBody @Valid List<CategoryDto.ModifyRequest> requests) {
         categoryService.modifyCategories(requests);
         return ResponseEntity.ok().build();
     }
