0a49796b4493c2cefd5c6deb1a8f8299a9221909
Ksinny <dosly2@naver.com>
2024-01-09 18:57:04 +0900

    Test: 단어장 API 테스트 케이스 추가 [#39]

--- dashboard/src/test/java/whale/dashboard/controller/VocabularyApiControllerTest.java
+++ dashboard/src/test/java/whale/dashboard/controller/VocabularyApiControllerTest.java
@@ -23,11 +23,12 @@ public class VocabularyApiControllerTest extends BaseControllerTest {
     @Test
     @DisplayName("단어장 등록")
     void registerVocabulary() throws Exception {
-        VocabularyDto.RegistrationRequest request = new VocabularyDto.RegistrationRequest("Test Title", "Test Description");
+        VocabularyDto.RegistrationRequest request1 = new VocabularyDto.RegistrationRequest("Test Title", "Test Description");
+        VocabularyDto.RegistrationRequest request2 = new VocabularyDto.RegistrationRequest("Test Title2", "Test Description2");
 
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/api/vocabulary")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(List.of(request))));
+                .content(objectMapper.writeValueAsString(List.of(request1, request2))));
 
         resultActions.andExpect(status().isOk());
     }
@@ -35,11 +36,12 @@ public class VocabularyApiControllerTest extends BaseControllerTest {
     @Test
     @DisplayName("단어장 수정")
     void modifyVocabulary() throws Exception {
-        VocabularyDto.ModifyRequest request = new VocabularyDto.ModifyRequest(1L, "Modified Title", "Modified Description");
+        VocabularyDto.ModifyRequest request1 = new VocabularyDto.ModifyRequest(1L, "Modified Title", "Modified Description");
+        VocabularyDto.ModifyRequest request2 = new VocabularyDto.ModifyRequest(2L, "Modified Title2", "Modified Description2");
 
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.patch("/api/vocabulary")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(List.of(request))));
+                .content(objectMapper.writeValueAsString(List.of(request1, request2))));
 
         resultActions.andExpect(status().isOk());
     }
