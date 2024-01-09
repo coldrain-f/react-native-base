512b7f6d89d208c3ca877428e7c35a9a4808c209
Ksinny <dosly2@naver.com>
2024-01-09 18:58:25 +0900

    Test: 카테고리 API 테스트 케이스 추가 [#39]

--- dashboard/src/test/java/whale/dashboard/controller/CategoryApiControllerTest.java
+++ dashboard/src/test/java/whale/dashboard/controller/CategoryApiControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import whale.dashboard.BaseControllerTest;
@@ -25,11 +24,12 @@ public class CategoryApiControllerTest extends BaseControllerTest {
   @DisplayName("카테고리 등록")
   void registerCategory() throws Exception {
       Long vocabularyId = 1L;
-      CategoryDto.RegistrationRequest request = new CategoryDto.RegistrationRequest("Test Subject", "Test Description");
+      CategoryDto.RegistrationRequest request1 = new CategoryDto.RegistrationRequest("Test Subject", "Test Description");
+      CategoryDto.RegistrationRequest request2 = new CategoryDto.RegistrationRequest("Test Subject2", "Test Description2");
 
       ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.post("/api/vocabulary/{id}/categories", vocabularyId)
               .contentType(MediaType.APPLICATION_JSON)
-              .content(objectMapper.writeValueAsString(List.of(request))));
+              .content(objectMapper.writeValueAsString(List.of(request1, request2))));
 
       resultActions.andExpect(status().isOk());
   }
@@ -37,11 +37,12 @@ public class CategoryApiControllerTest extends BaseControllerTest {
     @Test
     @DisplayName("카테고리 수정")
     void modifyCategory() throws Exception {
-        CategoryDto.ModifyRequest request = new CategoryDto.ModifyRequest(1L, "Modified Subject", "Modified Description", 2L);
+        CategoryDto.ModifyRequest request1 = new CategoryDto.ModifyRequest(1L, "Modified Subject", "Modified Description", 1L);
+        CategoryDto.ModifyRequest request2 = new CategoryDto.ModifyRequest(2L, "Modified Subject2", "Modified Description2", 1L);
 
         ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.patch("/api/categories")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(List.of(request))));
+                .content(objectMapper.writeValueAsString(List.of(request1, request2))));
 
         resultActions.andExpect(status().isOk());
     }
